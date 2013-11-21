b4de77afd448658fe6af93e7160f01ff3e985d3a
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2013-11-21 13:01:53 +0100

    Add pass-by-value and -reference examples

---
+++ functions-pointers/pass-by-value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void helper_function(int a) {
+  printf("%s(%d): a is %d\n", __func__, __LINE__, a);
+  a += 5;
+  printf("%s(%d): a is %d\n", __func__, __LINE__, a);
+}
+
+int main() {
+  int a = 5;
+  printf("%s(%d): a is %d\n", __func__, __LINE__, a);
+  helper_function(a);
+  printf("%s(%d): a is %d\n", __func__, __LINE__, a);
+}

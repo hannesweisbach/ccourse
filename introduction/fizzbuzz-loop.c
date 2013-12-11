e602ce6192065759612ea2eba548aab34aead8da
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2013-12-11 16:55:42 +0100

    Factor FizzBuzz logic into a function

--- introduction/fizzbuzz-loop.c
+++ introduction/fizzbuzz-loop.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fizzbuzz(int fizzbuzz) {
+    if(fizzbuzz % 3 == 0)
+        printf("Fizz");
+    if(fizzbuzz % 5 == 0)
+        printf("Buzz");
+    if((fizzbuzz % 3 != 0) && (fizzbuzz % 5 != 0))
+        printf("%d", fizzbuzz);
+     printf("\n");
+}
+
 int main() {
-    for(int fizzbuzz = 0; fizzbuzz <= 100; fizzbuzz++) {
-        if(fizzbuzz % 3 == 0)
-            printf("Fizz");
-        if(fizzbuzz % 5 == 0)
-            printf("Buzz");
-        if((fizzbuzz % 3 != 0) && (fizzbuzz % 5 != 0))
-            printf("%d", fizzbuzz);
-         printf("\n");
+    for(int i = 0; i <= 100; i++) {
+        fizzbuzz(i);
     }
     
     return EXIT_SUCCESS;

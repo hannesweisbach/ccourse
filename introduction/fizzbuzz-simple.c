7be52805fb5aa89458a1ddad19a36156236ee683
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2013-12-11 16:56:10 +0100

    Add fizzbuzz logic

--- introduction/fizzbuzz-simple.c
+++ introduction/fizzbuzz-simple.c
@@ -11,7 +11,13 @@ int main(int argc, char * argv[]) {
 
     sscanf(argv[1], "%d", &fizzbuzz);
 
-    /* ???? */
+    if(fizzbuzz % 3 == 0)
+        printf("Fizz");
+    if(fizzbuzz % 5 == 0)
+        printf("Buzz");
+    if((fizzbuzz % 3 != 0) && (fizzbuzz % 5 != 0))
+        printf("%d", fizzbuzz);
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
